4e09ed86645302b933ddd4278791dd581d859016
wanghe-fit2cloud <wanghe@fit2cloud.com>
2020-08-05 16:54:53 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# package
-KubeOpererator 3.0 离线安装包工程。
+# Kubernetes 离线安装包
+
+KubeOperator 提供完整的 Kubernetes 离线安装包，每个安装包会被构建成一个独立容器镜像供 KubeOperator 使用，离线安装包中包括以下三种内容：
+
+- 核心组件：包括 Kubenetes、Docker 及 etcd；
+- 镜像：包括网络、存储、监控、日志、管理工具及内置应用等的镜像；
+- RPM：除 CentOS 7.4+ Minimal 外的其他必备 RPM。
+
+本分支对应的 Kubernetes 离线包版本是 1.15.10，具体内容及版本信息如下所示。
+
+## 1. 核心组件
+
+|  分类  |  组件名称   | 版本  |
+|  ---- |  ----  | ----  |
+| 核心 | kubernetes  | 1.15.10 |
+| 核心 | etcd  | 3.3.10 |
+| 核心 | docker  | docker-ce-18.09.9 |
+
+## 2. 镜像列表
+
+离线包中镜像请查看: [requirements/images.txt](requirements/images.txt)
+
+## 3. RPM 列表
+
+离线包中 RPM 请查看: [requirements/rpms.txt](requirements/rpms.txt)
---
+++ build.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+save_dirname=/artifacts/ko-3.0/data/charts-kubeapps/images
+
+CURRENT_DIR=$(
+   cd "$(dirname "$0")"
+   pwd
+)
+
+os=`uname -a`
+
+# 仓库信息
+#local_hostname=172.16.10.65
+#repo_port=8082
+# k8s 版本
+#kube_version="v1.18.3"
+#kube_upgrade_version="v1.18.4"
+#wget_timeout=600
+
+# 判断系统架构
+if [[ $os =~ 'x86_64' ]];then
+  architectures=amd64
+  ansible_architecture=x86_64
+elif [[ $os =~ 'aarch64' ]];then
+  architectures=arm64
+  ansible_architecture=aarch64
+else
+  architectures=amd64
+  ansible_architecture=x86_64
+fi
+
+source ./requirements/tag_list
+source ./requirements/images_list
+source ./requirements/raw_list
+source ./requirements/rpms_list
+
+function prapre_dir() {
+  mkdir -p $CURRENT_DIR/{raw,images,rpms}
+  raw_save_dirname=/artifacts/ko-3.0/data/charts-kubeapps/raw
+  rpms_save_dirname=$CURRENT_DIR/rpms
+  images_save_dirname=$CURRENT_DIR/images
+}
+
+function download_image() {
+echo "+++++++++++++++++++++++++++++++++++下载 Docker 镜像++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  for image in ${K8S_IMAGES[@]}
+    do
+      image_name=`echo $image|awk -F= '{print $1}'`
+      echo "下载 ${image_name} >>"
+      docker pull `echo ${image}|awk -F= '{print $2}'`
+      docker rmi `echo $image|awk -F= '{print $2}'`
+    done
+  if [ ${architectures} = "amd64" ];then
+  for image in ${APPLICATION_IMAGES_AMD64[@]}
+    do
+      echo "下载 ${image} >>"
+      docker pull ${image}
+      docker rmi ${image}
+    done
+  elif [ ${architectures} = "arm64" ]; then
+  for image in ${APPLICATION_IMAGES_ARM64[@]}
+    do
+      echo "下载 ${image} >>"
+      docker pull ${image}
+      docker rmi ${image}
+    done
+  fi
+}
+
+function download_raw() {
+echo "+++++++++++++++++++++++++++++++++++下载 二进制文件++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+  for raw in ${RAW[@]}
+  do
+    raw_name=`echo $raw|awk -F= '{print $1}'`
+    echo "下载 ${raw_name} >>"
+    wget --timeout=${wget_timeout} -nv `echo $raw|awk -F= '{print $2}'` -P ${raw_save_dirname}
+  done
+}
+
+function download_rpm() {
+echo "+++++++++++++++++++++++++++++++++++下载 rpm ++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  yum clean all && yum makecache
+  for rpm in ${RPMS[@]}
+  do
+    rpm_name=`echo $rpm|awk -F= '{print $1}'`
+    echo "下载 ${rpm_name} >>"
+    yumdownloader --destdir=${rpms_save_dirname}  `echo $rpm|awk -F= '{print $1}'`
+  done
+}
+
+function main() {
+  prapre_dir
+  download_image
+  download_raw
+  download_rpm
+}
+
+main
