b24f1f1a46abbb7b88be617ec229981eef85ae21
wanghe-fit2cloud <wanghe@fit2cloud.com>
2021-03-02 17:52:58 +0800

    arm64 缓存 containerd 所需 raw 文件

--- data/arm64/raw.py
+++ data/arm64/raw.py
@@ -7,6 +7,9 @@ raw_url = {
     'etcd_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/etcd/{etcd_version}/{architectures}/etcd-{etcd_version}-linux-{architectures}.tar.gz',
     'cni_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/containernetworking/{cni_version}/{architectures}/cni-plugins-linux-{architectures}-{cni_version}.tgz',
     'docker_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/docker/{docker_version}/{architectures}/docker-{docker_version}.tgz',
+    'containerd_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/containerd/{containerd_version}/{architectures}/containerd-{containerd_version}-linux-{architectures}.tar.gz',
+    'crictl_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/crictl/{crictl_version}/{architectures}/crictl-{crictl_version}-linux-{architectures}.tar.gz',
+    'runc_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/runc/{runc_version}/{architectures}/runc.{architectures}',
     # ----------------------- 离线镜像文件下载地址 ----------------------
     'kube_controller_manager_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/k8s/{kube_version}/{architectures}/kube-controller-manager.tar',
     'kube_apiserver_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/k8s/{kube_version}/{architectures}/kube-apiserver.tar',
@@ -20,8 +23,6 @@ raw_url = {
     'kube_scheduler_upgrade_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/k8s/{kube_upgrade_version}/{architectures}/kube-scheduler.tar',
     'kube_proxy_upgrade_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/k8s/{kube_upgrade_version}/{architectures}/kube-proxy.tar',
     'pause_upgrade_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/k8s/{kube_upgrade_version}/{architectures}/pause.tar',
-    # ----------------------- 存储所需二进制文件下载地址 --------------------
-    'huawei_csi_passwdencrypt_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/oceanstor/{architectures}/passwdEncrypt',
     # ----------------------- helm 仓库地址 --------------------
     'helm3_download_url': 'http://{local_hostname}:{repo_port}/repository/oss-proxy/helm/{helm_v3_version}/helm-{helm_v3_version}-linux-{architectures}.tar.gz'
 }
