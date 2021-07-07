8aa03dc287e9e01654ad2ebfad333c2b1fcbdacf
xiaohu <564134281@qq.com>
2021-07-07 14:30:55 +0800

    feat(ubuntu): 更新apt源

--- data/util.py
+++ data/util.py
@@ -100,22 +100,22 @@ ubuntu_apt_amd64 = """
 deb http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version} main restricted universe multiverse
 deb http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-security main restricted universe multiverse
 deb http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-updates main restricted universe multiverse
-deb http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-backports main restricted universe multiverse
+# deb http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-backports main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version} main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-security main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-updates main restricted universe multiverse
-deb-src http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-backports main restricted universe multiverse
+# deb-src http://{ip}:8081/repository/apt-proxy-{ubuntu_version} {ubuntu_version}-backports main restricted universe multiverse
 """
 
 ubuntu_apt_arm64 = """
 deb http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version} main restricted universe multiverse
 deb http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-security main restricted universe multiverse
 deb http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-updates main restricted universe multiverse
-deb http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-backports main restricted universe multiverse
+# deb http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-backports main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version} main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-security main restricted universe multiverse
 deb-src http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-updates main restricted universe multiverse
-deb-src http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-backports main restricted universe multiverse
+# deb-src http://{ip}:8081/repository/apt-proxy-arm64-{ubuntu_version}  {ubuntu_version}-backports main restricted universe multiverse
 """
 
 
